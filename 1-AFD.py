d3d0322e05effdf2afc76fe05fd55146a27aca80
Hugo Silva <hugo20silva@hotmail.com>
2024-03-27 20:42:17 +0000

    atualizacao
    atualizacao

--- 1-AFD.py
+++ 1-AFD.py
@@ -1,68 +1,72 @@
 import json
-import graphviz
 import argparse
+import graphviz
 
-class Automato:
-    def __init__(self, ficheiro_definicao):
-        # Carrega a definição do automato a partir do ficheiro JSON
-        with open(ficheiro_definicao, 'r') as ficheiro:
-            self.definicao = json.load(ficheiro)
+def carregar_automato(ficheiro_definicao):
+    # Carrega o automato a partir do ficheiro JSON
+    with open(ficheiro_definicao, 'r') as ficheiro:
+        return json.load(ficheiro)
 
-    def reconhecer_palavra(self, palavra):
-        # Executa o automato para reconhecer a palavra
-        estado_atual = self.definicao['q0']
-        caminho = [estado_atual]
+def gerar_grafo(automato):
+    grafo = graphviz.Digraph(format='png')
 
-        # Percorre a palavra e transita entre os estados conforme as transições do automato
-        for char in palavra:
-            try:
-                prox_estado = self.definicao['delta'][estado_atual][char]
-                caminho.append(prox_estado)
-                estado_atual = prox_estado
-            except KeyError:
-                return False, caminho, f"Não há transição do estado {estado_atual} com o símbolo '{char}'."
-
-        # Verifica se o estado final alcançado é um estado final do automato
-        if estado_atual in self.definicao['F']:
-            return True, caminho, None
+    # Adiciona os estados
+    for estado in automato['Q']:
+        if estado in automato['F']:
+            grafo.node(estado, shape='doublecircle')
         else:
-            return False, caminho, f"O estado {estado_atual} não é um estado final."
+            grafo.node(estado)
 
-    def verificar_afd(self):
-        # Verifica se o automato carregado é um AFD válido
-        for estado, trans in self.definicao['delta'].items():
-            for simbolo, prox_estado in trans.items():
-                if isinstance(prox_estado, list):
-                    return False  # Se houver transição não determinística, não é um AFD
-        return True  # Se não houver transições não determinísticas, é um AFD válido
+    # Adiciona as transições
+    for estado, trans in automato['delta'].items():
+        for simbolo, prox_estado in trans.items():
+            grafo.edge(estado, prox_estado, label=simbolo)
+
+    return grafo
+
+def reconhecer_palavra(automato, palavra):
+    estado_atual = automato['q0']
+    caminho = [estado_atual]
+
+    for char in palavra:
+        try:
+            prox_estado = automato['delta'][estado_atual][char]
+            caminho.append(prox_estado)
+            estado_atual = prox_estado
+        except KeyError:
+            return False, caminho, f"Não há transição do estado {estado_atual} com o símbolo '{char}'."
+
+    if estado_atual in automato['F']:
+        return True, caminho, None
+    else:
+        return False, caminho, f"O estado {estado_atual} não é um estado final."
 
 def main():
-    # Configuração do parser de argumentos
-    # Terminal ------> python 1-AFD.py ficheiro.json palavra
     parser = argparse.ArgumentParser(description="Implementação de AFD")
     parser.add_argument('ficheiro_json', metavar='ficheiro_json', type=str,
                         help='O caminho para o ficheiro JSON contendo a definição do AFD')
-    parser.add_argument('palavra', metavar='palavra', type=str,
-                        help='A palavra a ser reconhecida pelo automato')
+    parser.add_argument('-graphviz', action='store_true',
+                        help='Gerar a representação gráfica do grafo')
+    parser.add_argument('-rec', metavar='palavra', type=str,
+                        help='Reconhecer uma palavra')
 
-    # Parse dos argumentos da linha de comando
     args = parser.parse_args()
 
-    # Carrega o AFD a partir do ficheiro JSON fornecido
-    automato = Automato(args.ficheiro_json)
+    automato = carregar_automato(args.ficheiro_json)
 
-    # Verifica se o automato é um AFD válido antes de executá-lo
-    if automato.verificar_afd():
-        print("O automato é um AFD válido.")
-        reconhecido, caminho, erro = automato.reconhecer_palavra(args.palavra)
+    if args.graphviz:
+        grafo = gerar_grafo(automato)
+        print(grafo.source)  # Imprime a representação do grafo em formato DOT
+        grafo.render('automato', view=True)  # Guarda e mostra o grafo em formato PNG
+
+    if args.rec:
+        reconhecido, caminho, erro = reconhecer_palavra(automato, args.rec)
         if reconhecido:
-            print(f"A palavra '{args.palavra}' é reconhecida.")
+            print(f"A palavra '{args.rec}' é reconhecida.")
             print("Caminho percorrido:", ' -> '.join(caminho))
         else:
-            print(f"A palavra '{args.palavra}' não é reconhecida.")
+            print(f"A palavra '{args.rec}' não é reconhecida.")
             print("Situação de erro:", erro)
-    else:
-        print("O automato não é um AFD válido.")
 
 if __name__ == "__main__":
     main()
