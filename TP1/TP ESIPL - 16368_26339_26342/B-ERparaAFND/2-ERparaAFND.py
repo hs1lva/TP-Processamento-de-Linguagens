ee65218d1b719e322d74a9b1e3d3fc4093f65d73
Hugo Silva <hugo20silva@hotmail.com>
2024-04-17 22:49:10 +0100

    update
    update

--- TP1/TP ESIPL - 16368_26339_26342/B-ERparaAFND/2-ERparaAFND.py
+++ TP1/TP ESIPL - 16368_26339_26342/B-ERparaAFND/2-ERparaAFND.py
@@ -2,13 +2,12 @@ import json
 import argparse
 
 # Função para converter uma expressão regular num automato finito não determinístico (AFND)
-# Args : expressao_regular (str) : expressão regular como uma string
+# Args : expressao_regular (dict) : expressão regular como um dicionário Python
 # Returns : dict : AFND como um dicionário Python
 def converter_afnd(expressao_regular):
 
     # Função para criar um novo estado único
     def criar_estado():
-        
         nonlocal contador_estado # Variável não local para manter o contador de estados
         estado = f"q{contador_estado}" # Cria um novo estado único
         contador_estado += 1 # Incrementa o contador de estados
@@ -19,7 +18,6 @@ def converter_afnd(expressao_regular):
     #        afnd2 (dict) : O segundo AFND como um dicionário Python
     # Returns : dict : O AFND resultante da concatenação
     def concatenacao(afnd1, afnd2):
-        
         # Adiciona transições vazias do estado final de afnd1 para o estado inicial de afnd2
         for estado_final in afnd1["F"]:
             afnd1["delta"].setdefault(estado_final, {}).update({"" : [afnd2["q0"]]})
@@ -37,7 +35,6 @@ def converter_afnd(expressao_regular):
     #        afnd2 (dict) : O segundo AFND como um dicionário Python
     # Returns : dict : O AFND resultante da união
     def uniao(afnd1, afnd2):
-        
         # Cria um novo estado inicial para o AFND resultante
         novo_estado_inicial = criar_estado()
 
@@ -59,7 +56,6 @@ def converter_afnd(expressao_regular):
     # Args : afnd (dict) : O AFND como um dicionário Python
     # Returns : dict : O AFND resultante do fecho de Kleene
     def kleene(afnd):
-        
         # Cria um novo estado inicial e um novo estado final para o AFND resultante
         novo_estado_inicial = criar_estado()
         novo_estado_final = criar_estado()
@@ -79,22 +75,28 @@ def converter_afnd(expressao_regular):
 
         return novo_afnd
 
-    # Função para realizar a operação de transição vazia num AFND, para usar o trans (transição vazia) na expressão regular
+    # Função para realizar a operação de transição transitiva num AFND, para usar o trans "+" na expressão regular
     # Args : afnd (dict) : O AFND como um dicionário Python
-    # Returns : dict : O AFND resultante da transição vazia
-    def transicao_vazia(afnd):
+    # Returns : dict : O AFND resultante da transição transitiva
+    def transitiva(afnd):
         novo_estado_inicial = criar_estado()
         novo_estado_final = criar_estado()
-        novo_delta = {"": [novo_estado_final]}
+        
+        novo_delta = {}
+        for estado in afnd["Q"]:
+            transicoes = afnd["delta"].get(estado, {})
+            transicoes.setdefault("", []).append(novo_estado_final)
+            novo_delta[estado] = transicoes
+        
         novo_afnd = {
             "Q": [novo_estado_inicial, novo_estado_final] + afnd["Q"],
             "Sigma": afnd["Sigma"],
-            "delta": {**novo_delta, **afnd["delta"]},
+            "delta": novo_delta,
             "q0": novo_estado_inicial,
             "F": [novo_estado_final]
         }
         return novo_afnd
-    
+
     # Inicializa o contador de estados
     contador_estado = 0
 
@@ -103,16 +105,14 @@ def converter_afnd(expressao_regular):
         "seq": concatenacao,
         "alt": uniao,
         "kle": kleene,
-        "trans": transicao_vazia  # Adiciona a operação de transição vazia - função transicao_vazia
+        "trans": transitiva
     }
 
     # Função recursiva para converter um estado da expressão regular num AFND
     # Args : estadoER (dict) : Um estado da expressão regular como um dicionário Python
     # Returns : dict : O AFND resultante do nó convertido
     def converter_estadoER(estadoER):
-        
         if "simb" in estadoER:  # Se o nó representa um símbolo então...
-
             # Cria dois novos estados para representar a transição com o símbolo
             estado_inicial = criar_estado()
             estado_final = criar_estado()
@@ -126,18 +126,14 @@ def converter_afnd(expressao_regular):
                 "q0": estado_inicial,
                 "F": [estado_final]
             }
-        
-        else:  # Se o nó representa um operador (seq, alt ou kle) então...
-
+        else:  # Se o nó representa um operador (seq, alt, kle ou trans) então...
             # Converte recursivamente os argumentos do operador para AFNDs
             sub_afnds = [converter_estadoER(arg) for arg in estadoER["args"]]
-
             # Aplica a operação correspondente ao operador aos AFNDs convertidos
             return operadores[estadoER["op"]](*sub_afnds)
 
     # Converte a expressão regular num AFND
     afnd = converter_estadoER(expressao_regular)
-
     return afnd
 
 def gravar_automato(automato: dict, ficheiro_automato: str) -> None:
@@ -145,18 +141,15 @@ def gravar_automato(automato: dict, ficheiro_automato: str) -> None:
         json.dump(automato, ficheiro, indent=4)
 
 def main():
-
     # Configuração do parser de argumentos
     parser = argparse.ArgumentParser(description="Conversão de expressão regular para AFND")
     parser.add_argument('er_file', metavar='er_file', type=str,
                         help='O ficheiro JSON com a expressão regular')
-    
     parser.add_argument('-output', metavar='palavra', type=str,
                         help='Output JSON do AFD')
-
     
     # Parse dos argumentos da linha de comando
-    args : argparse.Namespace = parser.parse_args()
+    args : argparse.ArgumentParser = parser.parse_args()
 
     # Ler a expressão regular do ficheiro JSON
     with open(args.er_file, 'r') as file:
@@ -171,12 +164,9 @@ def main():
     # Imprimir o AFND resultante (json.dumps converte o dicionário Python para uma string JSON)
     print(json.dumps(afnd, indent=4)) # indent=4 para imprimir o JSON de uma forma mais legível
 
-    #Grava o AFND num ficheiro 
+    # Grava o AFND num ficheiro 
     gravar_automato(afnd, args.output)
     print(f"Ficheiro criado")
 
 if __name__ == "__main__":
     main()
-
-# Exemplo de uso (dentro da pasta 2-ERparaAFND):
-# python 2-ERparaAFND.py er.json -output output.json
